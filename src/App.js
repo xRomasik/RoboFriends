bc0c608cdc30ddfb289e2d86119b12d48f8aef2d
xRomasik <xromasikx@gmail.com>
2020-11-17 23:01:44 +0100

    commitm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import Cardlist from './Cardlist';
+import Searchbox from './Searchbox';
+import Scroll from './Scroll';
 import './App.css';
+import ErrorBoundry from './ErrorBoundry';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+			
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+			return response.json();
+		})
+		.then(users => {
+			this.setState({ robots: users})
+		})
+		console.log(fetch('https://jsonplaceholder.typicode.com/users'))
+
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value})
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		if(this.state.robots.length === 0){
+			return <h1 className ='tc'>Loading...</h1>
+		} else {
+		return (
+		<div className ='tc'>
+			<h1>RoboFriends</h1>
+			<Searchbox searchChange = {this.onSearchChange} />
+			<Scroll>
+				<ErrorBoundry>
+					<Cardlist robots = {filteredRobots}/>
+				</ErrorBoundry>
+			</Scroll>
+		</div>
+		);
+
+		}
+		
+	}
 }
 
 export default App;
---
+++ src/Cardlist.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react'
+import Card from './Card'
+
+const Cardlist =({robots}) => {
+	return (
+		<Fragment>
+		 {	
+			robots.map((user, i) => {
+				return (
+					<Card 
+						key={robots[i].id} 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email} 
+					/>
+				);
+			})
+		}
+		</Fragment>
+	);
+}
+
+export default Cardlist;
